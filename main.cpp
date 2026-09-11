7c1c2dd0ff23ac20e8a4ad693ceb9e3ce1897369
Tristan King <tristanking@sheridan.edu>
2026-09-11 04:52:24 +0000

    Track Struct and functions prototypes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
+struct Track {
+    string title;
+    string artist;
+    string album;
+    int duration;
+
+    string genre;
+    int year;
+};
+
+// FunFunctional
+void addTrack(vector<Track>& library);
+void displayLibrary(const vector<Track>& library);
+void saveLibraryToFile(const vector<Track>& library, const string& filename);
+void loadLibraryFromFile(vector<Track>& library, const string& filename);
+void printMenu();
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
